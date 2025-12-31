6baa81ef78496744a85c38af5377db4f4191fdcd
thien-dev <dinhngocthien2004@gmail.com>
2025-12-31 21:45:10 +0700

    refactor the main code and greem 4ever

--- src/com/thien/mathutil/core/MathUtil.java
+++ src/com/thien/mathutil/core/MathUtil.java
@@ -23,20 +23,27 @@ public class MathUtil {
     // 10 giai thừa là 18 con số 0, vừa kịp đủ cho kiểu long của Java
     // 21 giao thừa tràn kiểu long
     // bài này quy ước tính n! trong khoảng 0...20
+//    public static long getFactorial(int n){
+//        
+//        if (n < 0 || n > 20)
+//            throw new IllegalArgumentException("Invalid argument. N must be between 0 ... 20");
+//        
+//        if (n == 0 || n == 1)
+//            return 1;  // kết thúc cuộc chơi sớm nếu nhận những đầu vào đặc biệt
+//        
+//        long product = 1; // tích nhân dồn, thuật toán heo đất, ốc bu dồn thịt.
+//        
+//        for (int i = 1; i <= n; i++) 
+//            product *= i;  //product = product * i;
+//        
+//        return product;
+//        
+//    }
+    
     public static long getFactorial(int n){
-        
-        if (n < 0 || n > 20)
-            throw new IllegalArgumentException("Invalid argument. N must be between 0 ... 20");
-        
-        if (n == 0 || n == 1)
-            return 1;  // kết thúc cuộc chơi sớm nếu nhận những đầu vào đặc biệt
-        
-        long product = 1; // tích nhân dồn, thuật toán heo đất, ốc bu dồn thịt.
-        
-        for (int i = 1; i <= n; i++) 
-            product *= i;  //product = product * i;
-        
-        return product;
-        
+//        if(n == 0 || n == 1)
+//            return 1;
+//        
+        return n * getFactorial(n - 1);
     }
 }
