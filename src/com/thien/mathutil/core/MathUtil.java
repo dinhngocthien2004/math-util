c64f14cbb6de65796df1d1e436d60fc4d3fc9a91
thien-dev <dinhngocthien2004@gmail.com>
2025-12-31 21:48:07 +0700

    refactor the main code and greem 4ever(1)

--- src/com/thien/mathutil/core/MathUtil.java
+++ src/com/thien/mathutil/core/MathUtil.java
@@ -41,9 +41,12 @@ public class MathUtil {
 //    }
     
     public static long getFactorial(int n){
-//        if(n == 0 || n == 1)
-//            return 1;
-//        
+        if(n < 0 || n > 20)
+            throw new IllegalArgumentException("Between 1 ... 20");
+        
+        if(n == 0 || n == 1)
+            return 1;
+        
         return n * getFactorial(n - 1);
     }
 }
